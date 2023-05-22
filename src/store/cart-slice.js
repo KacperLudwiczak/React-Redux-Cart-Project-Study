9cfda0d2a63a4dd67a20770c934243722c7d1fbb
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-22 22:03:45 +0200

    last change

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -5,6 +5,7 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
     replaceCart(state, action) {
@@ -15,6 +16,7 @@ const cartSlice = createSlice({
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
